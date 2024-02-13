918d10e0849dbce4bbd3425b4860e94dad8d9486
Hamed Rajabi Varamini <hamed.r.v@gmail.com>
2024-02-13 22:44:23 +0400

    Chore;Adds accessing an element

---
+++ accessing.py
@@ -0,0 +1,21 @@
+'''
+simple_programming_
+
+This code defines an array with 
+some elements and gets the value of elements
+'''
+langs = ["Python", "Java", "C++"]
+
+# Get single value
+lang1 = langs[0]
+lang2 = langs[1] 
+lang3 = langs[2]
+
+# Get multiple values
+l1, l2, l3 = langs
+
+print(lang1)
+print(lang2)
+print(lang3)
+print(l1, l2, l3)
+
